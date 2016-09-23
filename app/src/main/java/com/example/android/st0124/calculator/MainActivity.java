7f20132c872120b6181580f03e51cdebcf5d974f
ShotaTakai <takaishota@gmail.com>
2016-09-23 20:31:18 +0900

    first commit

---
+++ app/src/main/java/com/example/android/st0124/calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.st0124.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
